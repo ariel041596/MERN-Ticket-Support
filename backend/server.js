04bcd7fbe80cfd44554609211b6dd303e5eeb7a2
Espinoza <ariel.espinoza@accenture.com>
2022-06-25 09:04:18 +0800

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-console.log("Server file....");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+// Connect to Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to Service Desk System",
+  });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoute"));
+
+// Error Handler
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server is now running on ${PORT}`));
