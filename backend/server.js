ffbbccafa36c816392c7be074c03d7db75be8c18
Espinoza <ariel.espinoza@accenture.com>
2022-06-28 17:52:18 +0800

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -14,16 +15,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to Service Desk System",
-  });
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
-// app.use("/api/notes", require("./routes/noteRoutes")); // api/tickets/:ticketID/notes
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Welcome to Service Desk System",
+    });
+  });
+}
 
 // Error Handler
 app.use(errorHandler);
